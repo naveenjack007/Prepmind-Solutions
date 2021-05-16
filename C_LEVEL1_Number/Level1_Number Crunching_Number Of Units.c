720dde9f7d278acd757e54610d6b8b2e19701dd4
Naveen Natarajan <58937732+naveenjack007@users.noreply.github.com>
2021-05-16 13:48:59 +0530

    Update Level1_Number Crunching_Number Of Units.c

--- C_LEVEL1_Number/Level1_Number Crunching_Number Of Units.c
+++ C_LEVEL1_Number/Level1_Number Crunching_Number Of Units.c
@@ -1,15 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int a,n1,n2,n3,n4;
-    scanf("%d",&a);
-    n1=a;
-    n2=a/10;
-    n3=a/100;
-    n4=a/1000;
-    printf("Number of units : %d\n",n1);
-    printf("Number of tens : %d\n",n2);
-    printf("Number of hundreds : %d\n",n3);
-    printf("Number of thousands : %d\n",n4);
-
+    int n;
+    scanf("%d",&n);
+    printf("Number of units : %d\n",n);
+    printf("Number of tens : %d\n",n/10);
+    printf("Number of hundreds : %d\n",n/100);
+    printf("Number of thousands : %d\n",n/1000);
 }
